f23caaf49a0c9b43ca883de8f7fa9f4ff9f9e35b
risa1006 <risa@unida.ac.id>
2022-01-22 09:03:52 +0700

    UasJavaPattern2
    membuat java pattern 2

---
+++ src/JavaPattern2.java
@@ -0,0 +1,17 @@
+
+public class JavaPattern2 {
+			public static void main(String[] args) {
+				int a, b,c;
+				for (a=8;a>=1;a--) {
+					
+					for (b=8;b>a ; b--) {
+					}
+					for (c=1; c<=b; c++) {
+						System.out.print("* ");
+					}
+					System.out.println(); 
+				}
+			}
+	}
+
+
